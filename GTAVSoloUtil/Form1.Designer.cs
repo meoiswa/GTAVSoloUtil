3b30154c9df30f554b59cef92b536f3fbb545147
G752VY\meoiswa <meoiswa@gmail.com>
2017-02-16 14:34:47 -0400

    Initial Version

--- GTAVSoloUtil/Form1.Designer.cs
+++ GTAVSoloUtil/Form1.Designer.cs
@@ -29,11 +29,147 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSuspend = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.backgroundWorker = new System.ComponentModel.BackgroundWorker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.maximizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Status:";
+            // 
+            // buttonSuspend
+            // 
+            this.buttonSuspend.Enabled = false;
+            this.buttonSuspend.Location = new System.Drawing.Point(15, 99);
+            this.buttonSuspend.Name = "buttonSuspend";
+            this.buttonSuspend.Size = new System.Drawing.Size(247, 23);
+            this.buttonSuspend.TabIndex = 2;
+            this.buttonSuspend.Text = "Suspend";
+            this.buttonSuspend.UseVisualStyleBackColor = true;
+            this.buttonSuspend.Click += new System.EventHandler(this.buttonSuspend_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(15, 128);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(247, 23);
+            this.buttonExit.TabIndex = 3;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.AutoSize = true;
+            this.labelStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatus.Location = new System.Drawing.Point(58, 9);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(28, 13);
+            this.labelStatus.TabIndex = 4;
+            this.labelStatus.Text = "Idle";
+            // 
+            // backgroundWorker
+            // 
+            this.backgroundWorker.WorkerReportsProgress = true;
+            this.backgroundWorker.WorkerSupportsCancellation = true;
+            this.backgroundWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker_DoWork);
+            this.backgroundWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker_ProgressChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(250, 65);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "How to use:\r\n\r\nPress the Pause/Break key in a public online lobby.\r\nThe game will" +
+    " freeze for 10 seconds.\r\nAfter it resumes, you should be alone.";
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyIcon1.BalloonTipText = "Minimized to Tray";
+            this.notifyIcon1.BalloonTipTitle = "GTAV Public Solo Utility";
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "GTAV Public Solo Utility";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.maximizeToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(125, 48);
+            // 
+            // maximizeToolStripMenuItem
+            // 
+            this.maximizeToolStripMenuItem.Name = "maximizeToolStripMenuItem";
+            this.maximizeToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.maximizeToolStripMenuItem.Text = "Maximize";
+            this.maximizeToolStripMenuItem.Click += new System.EventHandler(this.maximizeToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(124, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(275, 161);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonSuspend);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "GTAV Public Solo Utility";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSuspend;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label labelStatus;
+        private System.ComponentModel.BackgroundWorker backgroundWorker;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem maximizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- GTAVSoloUtil/Form1.cs
+++ GTAVSoloUtil/Form1.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -12,9 +14,184 @@ namespace GTAVSoloUtil
 {
     public partial class Form1 : Form
     {
+        KeyHandler ghk;
+
+        string processName = "GTA5";
+
+        Process process;
+        Status status = Status.zero;
+
+        enum Status
+        {
+            zero,
+            search,
+            ready,
+            suspend,
+            waiting,
+            error
+        }
+
         public Form1()
         {
             InitializeComponent();
+            ghk = new GTAVSoloUtil.KeyHandler(Keys.Pause, this);
+            ghk.Register();
+        }
+
+        private delegate bool StateChecker();
+
+        private void Exit()
+        {
+            backgroundWorker.CancelAsync();
+            while (backgroundWorker.IsBusy)
+            {
+                Application.DoEvents();
+                Thread.Sleep(10);
+            }
+
+            Application.Exit();
+        }
+
+        private void Maximize()
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason == CloseReason.UserClosing)
+            {
+                e.Cancel = true;
+                notifyIcon1.ShowBalloonTip(0);
+                Hide();
+            }
+        }
+
+        private void HandleHotkey()
+        {
+            if (status == Status.ready)
+            {
+                status = Status.suspend;
+            }
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == Constants.WM_HOTKEY_MSG_ID)
+                HandleHotkey();
+            base.WndProc(ref m);
+        }
+
+        private void backgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            BackgroundWorker worker = (BackgroundWorker) sender;
+
+            int sleepTime = 0;
+
+            while(!worker.CancellationPending)
+            {
+                if (process == null || process.HasExited)
+                {
+                    status = Status.search;
+                    process = Process.GetProcessesByName(processName).FirstOrDefault();
+                }
+                else
+                {
+                    if (status == Status.search)
+                    {
+                        status = Status.ready;
+                    }
+                    else if (status == Status.suspend)
+                    {
+                        process.Suspend();
+                        status = Status.waiting;
+                        sleepTime = 0;
+                    }
+                    else if (status == Status.waiting)
+                    {
+                        if (sleepTime < 100)
+                        {
+                            sleepTime += 1;
+                        }
+                        else
+                        {
+                            process.Resume();
+                            status = Status.ready;
+                        }
+                    }
+                    
+                }
+                worker.ReportProgress(sleepTime, status);
+                Thread.Sleep(100);
+            }
+            if (process != null && !process.HasExited)
+            {
+                process.Resume();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            backgroundWorker.RunWorkerAsync();
+        }
+
+        private void backgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            string text = "Error";
+            switch ((Status) e.UserState)
+            {
+                case Status.zero:
+                    text = "Idle";
+                    buttonSuspend.Enabled = false;
+                    break;
+                case Status.search:
+                    text = "Searching";
+                    buttonSuspend.Enabled = false;
+                    break;
+                case Status.ready:
+                    text = "Ready";
+                    buttonSuspend.Enabled = true;
+                    break;
+                case Status.suspend:
+                    text = "Suspending";
+                    buttonSuspend.Enabled = false;
+                    break;
+                case Status.waiting:
+                    text = "Waiting ("+e.ProgressPercentage/10+")";
+                    buttonSuspend.Enabled = false;
+                    break;
+            }
+            labelStatus.Text = text;
+        }
+
+        private void buttonSuspend_Click(object sender, EventArgs e)
+        {
+            if (status == Status.ready)
+            {
+                status = Status.suspend;
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Exit();
+        }
+
+        private void notifyIcon1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Maximize();
+        }
+
+        private void maximizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Maximize();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Exit();
         }
     }
 }
